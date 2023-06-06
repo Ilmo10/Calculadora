5207309491bad1d3de03d498262ad87a127ef62d
Ilmo10 <necok_cley@hotmail.com>
2023-06-06 15:20:56 -0300

    finalizado calculadora projeto Udemy

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -203,7 +203,15 @@ class CalcController {
 
     getResult(){
 
-        return eval(this._operation.join(""));
+        try{
+            return eval(this._operation.join(""));
+        } catch(e){
+            setTimeout(()=>{
+                this.setError();
+            }, 1);
+        }
+
+
 
     }
     calc(){
